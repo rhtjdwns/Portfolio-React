2fab75b6a32919770b4c0df1e26a8a32beb45365
rhtjdwns <ib041615@naver.com>
2025-02-26 19:29:31 +0900

    인트로 영역

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+    const [show, setShow] = useState(false);
+
+    const toggleMenu = () => {
+        setShow((prevShow) => !prevShow);
+    }
+
     return (
         <header id="header" role="banner">
             <div className="header__inner">
                 <div className="header__logo">
-                    <a href="/">portfolio<em>vite</em></a>
+                    <a href="/">portfolio<em>react.js</em></a>
                 </div>
                 <nav className="header__nav" role="navigation" aria-label="메인 메뉴">
                     <ul>
+                        {headerNav.map((nav, key) => (
+                            <li key={key}>
+                                <a href={nav.url}>{nav.title}</a>
+                            </li>
+                        ))}
+
                         <li><a href="#intro">intro</a></li>
                         <li><a href="#skill">skill</a></li>
                         <li><a href="#site">site</a></li>
@@ -30,5 +42,29 @@ const Header = () => {
         </header>
     );
 };
+
+/* 데이터를 따로 작업하여 관리함으로써 코드의 가독성과 유지보수성을 향상. */
+const headerNav = [
+    {
+        title: "intro",
+        url: "#intro",
+    },
+    {
+        title: "skill",
+        url: "#skill",
+    },
+    {
+        title: "site",
+        url: "#site",
+    },
+    {
+        title: "portfolio",
+        url: "#port",
+    },
+    {
+        title: "contact",
+        url: "#contact",
+    },
+]
 
 export default Header;
