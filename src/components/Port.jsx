47a7fa93275ccf66de647a8f4ded78f6b77a4eaa
rhtjdwns <ib041615@naver.com>
2025-02-26 20:51:36 +0900

    포폴 영역

--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -1,7 +1,58 @@
 import React from "react";
 
+const portText = [
+    {
+        num: "01",
+        title: "React를 이용한 포트폴리오",
+        code: "/",
+        view: "https://github.com/rhtjdwns/Portfolio-React",
+    },
+    {
+        num: "02",
+        title: "React를 이용한 포트폴리오",
+        code: "/",
+        view: "",
+    },
+    {
+        num: "03",
+        title: "React를 이용한 포트폴리오",
+        code: "/",
+        view: "",
+    },
+    {
+        num: "04",
+        title: "React를 이용한 포트폴리오",
+        code: "/",
+        view: "",
+    },
+    {
+        num: "05",
+        title: "React를 이용한 포트폴리오",
+        code: "/",
+        view: "",
+    },
+]
+
 const Port = () => {
-    return <div>Port</div>;
+    return (
+        <section id="port">
+            <div className="port__inner">
+                <div className="port__title">
+                    portfolio <em>포트폴리오 작업물</em>
+                </div>
+                <div className="port__wrap">
+                    {portText.map((port, key) => (
+                        <article className={`port__item p${key + 1}`} key={key}>
+                            <span className="num">{port.num}</span>
+                            <a href={port.code} target="_blank" className="img" rel="noreferrer"></a>
+                            <h3 className="title">{port.title}</h3>
+                            <a href={port.view} target="_blank" className="site" rel="noreferrer">사이트 보기</a>
+                        </article>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Port;
