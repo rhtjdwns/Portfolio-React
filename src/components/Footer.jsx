561ec42c98a7f3b9bedf6ad65599536f6eaa0152
rhtjdwns <ib041615@naver.com>
2025-02-28 10:08:08 +0900

    푸터 영역

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
+const footerText = [
+    {
+        title: "github",
+        desc: "깃허브 주소",
+        link: "",
+    },
+    {
+        title: "github",
+        desc: "깃허브 주소",
+        link: "",
+    },
+]
+
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer id="footer" role="contentinfo">
+            <div className="footer__inner">
+                <div className="footer__text">
+                    <span>webstoryboy</span>
+                    <span>© webs</span>
+                </div>
+                <div className="footer__info">
+                    <div className="left">
+                        <div className="title">
+                            <a href="/">sign up</a>
+                        </div>
+                        <p>회원가입을 하시면 댓글을 달 수 있습니다.</p>
+                    </div>
+                    <div className="right">
+                        <h3>social</h3>
+                        <ul>
+                            {footerText.map((footer, key) => (
+                                <li key={key}>
+                                    <a href={footer.link}>{footer.title}</a>
+                                    <em>{footer.desc}</em>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
